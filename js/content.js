105fa67dfdbd0d618d7af398bb55d529794cf3f6
崎轩 <chuncheng.licc@alibaba-inc.com>
2013-07-16 23:17:19 +0800

    dev

--- js/content.js
+++ js/content.js
@@ -3,71 +3,62 @@
 // found in the LICENSE file.
 
 
-var URLParser = function(url) {  
+var URLParser = function(url) {
 
-    this._fields = {  
-        'Username' : 4,   
-        'Password' : 5,   
-        'Port' : 7,   
-        'Protocol' : 2,   
-        'Host' : 6,   
-        'Pathname' : 8,   
-        'URL' : 0,   
-        'Querystring' : 9,   
-        'Fragment' : 10  
-    };  
+    this._fields = {
+        'Username' : 4,
+        'Password' : 5,
+        'Port' : 7,
+        'Protocol' : 2,
+        'Host' : 6,
+        'Pathname' : 8,
+        'URL' : 0,
+        'Querystring' : 9,
+        'Fragment' : 10
+    };
 
-    this._values = {};  
-    this._regex = null;  
-    this.version = 0.1;  
-    this._regex = /^((\w+):\/\/)?((\w+):?(\w+)?@)?([^\/\?:]+):?(\d+)?(\/?[^\?#]+)?\??([^#]+)?#?(\w*)/;  
-    for(var f in this._fields)  
-    {  
-        this['get' + f] = this._makeGetter(f);  
-    }  
+    this._values = {};
+    this._regex = null;
+    this.version = 0.1;
+    this._regex = /^((\w+):\/\/)?((\w+):?(\w+)?@)?([^\/\?:]+):?(\d+)?(\/?[^\?#]+)?\??([^#]+)?#?(\w*)/;
+    for(var f in this._fields)
+    {
+        this['get' + f] = this._makeGetter(f);
+    }
 
-    if (typeof url != 'undefined')  
-    {  
-        this._parse(url);  
-    }  
-}  
-URLParser.prototype.setURL = function(url) {  
-    this._parse(url);  
-}  
+    if (typeof url != 'undefined')
+    {
+        this._parse(url);
+    }
+}
+URLParser.prototype.setURL = function(url) {
+    this._parse(url);
+}
 
-URLParser.prototype._initValues = function() {  
-    for(var f in this._fields)  
-    {  
-        this._values[f] = '';  
-    }  
-}  
+URLParser.prototype._initValues = function() {
+    for(var f in this._fields)
+    {
+        this._values[f] = '';
+    }
+}
 URLParser.prototype.setValue = function(type, value) {
 	this._values[type] = value;
 }
 
-URLParser.prototype.getURL = function() {
-	var url = '';
-	for(var f in this._fields) if (typeof this._values[f] != 'undefined')  
-    {  
-        url += this._values[f]; 
-    }  
-	console.log(url);
+URLParser.prototype._parse = function(url) {
+    this._initValues();
+    var r = this._regex.exec(url);
+    if (!r) throw "DPURLParser::_parse -> Invalid URL";
+
+    for(var f in this._fields) if (typeof r[this._fields[f]] != 'undefined')
+    {
+        this._values[f] = r[this._fields[f]];
+    }
 }
-
-URLParser.prototype._parse = function(url) {  
-    this._initValues();  
-    var r = this._regex.exec(url);  
-    if (!r) throw "DPURLParser::_parse -> Invalid URL";  
-
-    for(var f in this._fields) if (typeof r[this._fields[f]] != 'undefined')  
-    {  
-        this._values[f] = r[this._fields[f]];  
-    }  
-}  
-URLParser.prototype._makeGetter = function(field) {  
-    return function() {  
-        return this._values[field];  
-    }  
+URLParser.prototype._makeGetter = function(field) {
+    return function() {
+        return this._values[field];
+    }
 }
 
 
@@ -75,35 +66,41 @@ URLParser.prototype._makeGetter = function(field) {
 var storage = chrome.storage.local;
 var css_link = [];
 var init = function(){
-	css_link = document.querySelectorAll("link");
-	console.log(css_link);
-	if(!css_link.length) return;
-	css_link = [].slice.call(css_link);
-	storage.get('result', function(data){
-		if(!data['result']) return;
-		if(!data.result.length) return;
-		act(data.result);	
-	});
+    storage.get('status', function(data){
+        if(data.status === 'on'){
+            css_link = document.querySelectorAll("link");
+            if(!css_link.length) return;
+            css_link = [].slice.call(css_link);
+            storage.get('result', function(data){
+                if(!data['result']) return;
+                if(!data.result.length) return;
+                act(data.result);
+            });
+        }
+    });
+
 }
 
 var act = function(data){
 	css_link.forEach(function(item){
-		var url = new URLParser(item.href);
+        var href = item.href;
+		var url = new URLParser(href);
 		var host = url.getHost();
-		var new_host = checkStat(data, host), new_url;
-		if(new_host){
-			url.setValue('Host', new_host);
-			url.getURL();
+        var new_host = checkStat(data, host);
+        if(new_host){
+           href = href.replace(new_host.remote_host, new_host.proxy_host);
+           href = href.replace(new_host.remote_path, new_host.proxy_path);
+           href = href.replace('-min', '');
+           item.href = href;
 		}
-
 	});
 }
 
 var checkStat = function(data, host){
 	var res = false;
 	data.forEach(function(item){
-		if(item.ck && item.map1 === host){
-			res = item.map2;
+		if(item.ck && item.remote_host === host){
+			res = item;
 			return res;
 		}
 	});
--- popup.js
+++ popup.js
@@ -3,12 +3,13 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 var storage = chrome.storage.local || false;
-var result = [], msg, add, items, save;
+var result = [], msg, add, items, save, status;
 function init(){
 	add = document.querySelector("#Add");
 	items = document.querySelector("#ItemList");
 	save = document.querySelector("#Save");
 	msg = document.querySelector("#Msg");
+	statuses = document.querySelector("#Status");
 	add.addEventListener("click", function(){
 		addItem(items);
 	});
@@ -16,14 +17,18 @@ function init(){
 	save.addEventListener("click", function(){
 		doSave(items);
 	});
+	statuses.addEventListener("click", function(){
+		changeStatus(statuses);
+	})
 	renderLit(items);
+	setStatus(statuses);
 }
 
 // 添加一项映射表单
 var addItem = function(d){
 	var tpl = document.createElement("div");
 	tpl.className = "item";
-	tpl.innerHTML = '<input type="checkbox" class="ck"/><input type="text" class="map1 text" placeholder="g.tbcdn.cn"/><span>Map -></span><input type="text" class="map2 text" placeholder="minint-quod3i0"/>';
+	tpl.innerHTML = '<input type="checkbox" class="ck"/><input type="text" class="map1 text" placeholder="g.tbcdn.cn"/><input type="text" class="map2 text" placeholder="/ju/dingzhi/1.0.2"/><span>Map -></span><input type="text" class="map3 text" placeholder="minint-quod3i0"/><input type="text" class="map4 text" placeholder="local path"/>';
 	d.appendChild(tpl);
 };
 
@@ -36,11 +41,15 @@ var doSave = function(d){
 	item.forEach(function(l){
 		var map1 = l.querySelector('.map1'),
 			map2 = l.querySelector('.map2'),
+			map3 = l.querySelector('.map3'),
+			map4 = l.querySelector('.map4'),
 			ck = l.querySelector('.ck');
-		if(map1.value && map2.value){
+		if(map1.value && map3.value){
 			result.push({
-				map1: map1.value,
-				map2: map2.value,
+				remote_host: map1.value,
+				remote_path: map2.value,
+				proxy_host: map3.value,
+				proxy_path: map4.value,
 				ck: ck.checked
 			});
 		}
@@ -58,7 +67,7 @@ var tip = function(t, d){
 	}
 };
 var renderLit = function(d){
-	
+
 	storage.get('result', function(data){
 		if(!data['result']) return;
 		if(!data.result.length) return;
@@ -73,11 +82,33 @@ var renderLit = function(d){
 			}else{
 				tmp += '<input type="checkbox" class="ck"/>';
 			}
-			tmp += '<input type="text" class="map1 text" value="' + l.map1 + '" placeholder="g.tbcdn.cn"/><span>Map -></span><input type="text" class="map2 text" value="' + l.map2 + '" placeholder="minint-quod3i0"/>';
+			l.remote_host = l.remote_host || '';
+			l.remote_path = l.remote_path || '';
+			l.proxy_host = l.proxy_host || '';
+			l.proxy_path = l.proxy_path || '';
+			tmp += '<input type="text" class="map1 text" value="' + l.remote_host + '" placeholder="g.tbcdn.cn"/><input type="text" class="map2 text" value="'+l.remote_path+'"placeholder="/ju/dingzhi/1.0.2"/><span>Map -></span><input type="text" class="map3 text" value="' + l.proxy_host + '" placeholder="minint-quod3i0"/><input type="text" class="map4 text" value="' + l.proxy_path + '" placeholder="local path"/>';
 			tmp += '</div>';
 
 			dpl += tmp;
 		});
 		d.innerHTML = dpl;
 	});
+}
+
+var setStatus = function(d){
+	storage.get('status', function(data){
+		if(data.status === 'on'){
+			d.className = 'on';
+		}else{
+			d.className = 'off';
+		}
+	});
+}
+var changeStatus = function(d){
+	if(d.className === 'off'){
+		d.className = 'on';
+	}else{
+		d.className = 'off';
+	}
+	storage.set({status: d.className});
 }
